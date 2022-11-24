3f8f4af44d0013f5851fbaec0f2d7620589caabb
klausHe <klausheuken@gmx.de>
2022-11-24 19:31:31 +0100

    new test for vercel

--- server.js
+++ server.js
@@ -22,10 +22,20 @@ App.listen(process.env.PORT);
 App.get('/', (req, res) => res.redirect(redirectPath));
 console.log(`Started @ Port: ${process.env.PORT}`)
 
-const ServiceAccount = require("./serviceAccountKey.json");
 const Admin = require("firebase-admin");
 Admin.initializeApp({
-  credential: Admin.credential.cert(ServiceAccount),
+  credential: Admin.credential.cert({
+    "type": "service_account",
+    "project_id": "kad-universe",
+    "private_key_id": "3d196bf43855666f27a8ffa189798a89a9dc9e75",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC7acwMmLtpOzPd\nr3SmtbopNDB0NtYPRb9Xf+89JQeoKOJUacQBRQId+PqLyXmtBIBa1lgcaQq+HEQ3\nZ40/orS8oorYDwJFRnwoSI3Tb/VYRql9JXcnP9Bzftza3IENqkcOHjOQ0MWDjoZW\npiiO+8TdPJB4yZfSS/k4C6yrQX1mWsrNB21+Tyj/OaggEVpD1xyq0CStMLjgw4sF\nncL0egLLgtWVaiWALEPoRbgdLgrlqcYW5gx4ZHzh/qJYbikuw749uruNOY4Op5le\n9OdxjCldIsXgmxDrCrS3uGLQujPUmHCBS3JrBUc8QbPMY2zY2OdM7xv2QXkuP9NR\nt4MDDQn1AgMBAAECggEAFlRiMAQInF6l7N1pYC2Z2yvxM/+OnK46QJIpLpdZZLNO\nWDFr3ZzbxjdynrIoMbGuG2SbhfMMCR+0Dwfi//iCYVFaiqirJBbkVyZh45Xc3SI6\nMoOCwWXsj75HUTphk/+8TJXTngcJ6YVTQ0Kz5WlhCN0c949JbBCKRp+5eIQqjUXC\nvFmU7mVEzreKehe9kYgl2yns5xf/gyKyOAy70vDnWD/Wg3ahx5G1GZssYgpa6AyW\nUjJ0jkWS/C4qWp1maA5k7xJDOcudNeOO3jDnGF/Wjtwsy3ziByufIxuMPoLWynT8\neuudev4AvlLsZu+wCc53aNMW6q3ByRKNyltoKS+fUQKBgQDrW8ZjHsLAb+JH8Ssf\nVcZPKUvrTU2cxnLWAzCRhJD3nMFCDQF0FCilyd/2Yr4Uh4EEzt3vJhvghlZEILi3\n9pU5d0R7odElS3z1k2N+fq/gR9ermALqKHT9KVVj6+wQ9AYVf/AbPKkIAKI2y70J\npOVGpjgU7FvUbfqzPOFnpDzE/QKBgQDL2ZCZmCUUnW1mlpcqFgXwMsp/AkHOHqs2\n2HasC+jFpB6kyL6MZrx4GDEz/h0d8TL89O36Dc7wIZdFB5rrLif2Rvz8RClY9cLV\ns6zoMZirSJPY1v0+JnmkpfPxK+FBmm+bDa5F3gFgUYSMVYKrWJaowYQ8Z0uSmAgu\n0jHe9hsmWQKBgCY0GXnRbm9qOeLPpN5LQAtteqNRQv3lwJI4plOYnydyTHR6Q/gh\npY9zu2T1FkGi1TCAm8nL9850P+CDJzjFT+eqEnW3CInzPO9n40iPK+FOCBYUcLSt\nqH/Hi9LSYtlFow8uWz0wlYd1At++JotANu9G3+eVjjpnN8CMa1I1eJ/JAoGAB3sL\ncGeB7Ni+HAaos3WtcBwdDD4j9hFwDQoyiKvDNgWIzllDjNl0Tx/sJlFL9j5Uta2z\nUfrRvkQtTe4yhDDkbIg8XnGN5uLV7Iu0a6zuQlLeghWZo3879YvVgOMwV/z33YPK\nAmxcNpGnQJMewXT1ymmXlbPgtRd5LWavR9RWOLkCgYBqebmnmu6rRB30XR5ff38m\nYkAasMuW5GrY1AYGxmnX3TudOM0yU2BVuNeGO65hZ1wF8hy4Hs8jncqQVppt6ArY\nUI1FJ0j5iLL4ppEfBqBHQjo9vI0kW8x7W15MAE6scUCD+UE2O1SbggEYdDXbWLoR\ncXRQatCMgTf4WBm9kXkVDg==\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-mf5ra@kad-universe.iam.gserviceaccount.com",
+    "client_id": "116420337746827807785",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-mf5ra%40kad-universe.iam.gserviceaccount.com"
+  }),
   databaseURL: "https://kad-universe.firebaseio.com"
 });
 let FBData = Admin.firestore();
