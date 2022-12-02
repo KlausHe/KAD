0d4d5d5b7f92615900f63a959534856542c3f53c
klausHe <klausheuken@gmx.de>
2022-12-02 12:39:24 +0100

    postillion to array

--- K-Universe/News/PostillonTicker.js
+++ K-Universe/News/PostillonTicker.js
@@ -1,48 +1,49 @@
 let postillonTickerOptions = {
-  tickerNums: null,
-  num: null,
-  usedNums: [],
-  get availiableNums() {
-    return this.tickerNums.filter((obj) => {
-      return !this.usedNums.includes(obj)
-    })
-  },
-  data: null
+	tickerNums: null,
+	num: null,
+	usedNums: [],
+	get availiableNums() {
+		return this.tickerNums.filter((obj) => {
+			return !this.usedNums.includes(obj);
+		});
+	},
+	data: null,
 };
 
 function clear_cl_PostillonTicker() {
-  if (postillonTickerOptions.data == null) {
-    globalP5.loadJSON("./Data/DataLists/Newsticker.json", tickerLoadData, 'json');
-  }
-  postillonTickerOptions.usedNums = [];
-};
+	if (postillonTickerOptions.data == null) {
+		globalP5.loadJSON("./Data/DataLists/Newsticker.json", tickerLoadData, "json");
+	}
+	postillonTickerOptions.usedNums = [];
+}
 
 function tickerLoadData(data) {
-  postillonTickerOptions.data = data;
-  postillonTickerOptions.tickerNums = Object.keys(data).map((n) => Number(n));
-  newsTickerNext();
+	postillonTickerOptions.data = data.arr;
+	postillonTickerOptions.tickerNums = new Array(postillonTickerOptions.data.length).fill(0).map((n, i) => i);
+	console.log(postillonTickerOptions.tickerNums, postillonTickerOptions.data);
+	newsTickerNext();
 }
 
 function newsTickerNext() {
-  postillonTickerOptions.num = randomObject(postillonTickerOptions.availiableNums);
-  postillonTickerOptions.usedNums.push(postillonTickerOptions.num);
-  createPostillion();
-};
+	postillonTickerOptions.num = randomObject(postillonTickerOptions.availiableNums);
+	postillonTickerOptions.usedNums.push(postillonTickerOptions.num);
+	createPostillion();
+}
 
 function createPostillion() {
-  dbID("idLbl_newsTickerNumber").innerHTML = `Ticker ${postillonTickerOptions.num}`;
-  clearTable("idTabBody_postillionTable");
-  const list = postillonTickerOptions.data[postillonTickerOptions.num]
-  for (let i = 0; i < list.length; i++) {
-    const row = insertTableRow("idTabBody_postillionTable");
-    tableAddCell(row, {
-      names: ["postillon", i],
-      type: "Lbl",
-      text: `${list[i]}`,
-      cellStyle: {
-        textAlign: "center"
-      },
-      copy: true
-    });
-  };
-};
+	dbID("idLbl_newsTickerNumber").innerHTML = `Ticker ${postillonTickerOptions.num}`;
+	clearTable("idTabBody_postillionTable");
+	const list = postillonTickerOptions.data[postillonTickerOptions.num];
+	for (let i = 0; i < list.length; i++) {
+		const row = insertTableRow("idTabBody_postillionTable");
+		tableAddCell(row, {
+			names: ["postillon", i],
+			type: "Lbl",
+			text: `${list[i]}`,
+			cellStyle: {
+				textAlign: "center",
+			},
+			copy: true,
+		});
+	}
+}
