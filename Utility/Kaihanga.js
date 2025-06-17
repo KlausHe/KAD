a9f696c52a434dbbf827579b13ee35293ef62c90
klausHe <klausheuken@gmx.de>
2025-06-17 21:58:18 +0200

    try to fix buildaction

--- Utility/Kaihanga.js
+++ Utility/Kaihanga.js
@@ -22,6 +22,7 @@ export function clear_cl_Kaihanga() {
   kaihangaOptions.colStart = KadRandom.randomIndex(globalColors.colorOptions);
   kaihangaOptions.entries = [];
   kaihangaOptions.spinning = false;
+  kaihangaOptions.wheel = null;
   KadTable.clear("idTabBody_Kaihanga");
   caKA.noLoop();
   caKA.clear();
@@ -36,68 +37,6 @@ export function clear_cl_Kaihanga() {
 export function canvas_cl_Kaihanga() {
   caKA.resizeCanvas(kaihangaOptions.canvas.w, kaihangaOptions.canvas.h);
   clear_cl_Kaihanga();
-}
-
-const caKA = new p5((c) => {
-  c.setup = function () {
-    c.canv = c.createCanvas(kaihangaOptions.canvas.w, kaihangaOptions.canvas.h);
-    c.canv.id("canvasKaihanga");
-    c.canv.parent("#idCanv_kaihanga");
-    c.canv.mousePressed(mousePushedKaihanga);
-    c.colorMode(c.HSL);
-    c.angleMode(c.DEGREES);
-    c.noLoop();
-  };
-
-  c.draw = function () {
-    if (kaihangaOptions.entries.length > 0) {
-      kaihangaOptions.wheel.update();
-      kaihangaOptions.wheel.draw();
-    }
-  };
-}, "#idCanv_kaihanga");
-
-function mousePushedKaihanga() {
-  let speed = caKA.floor(caKA.map(caKA.mouseX, 0, kaihangaOptions.canvas.w, 1, 18));
-  kaihangaStart(speed);
-}
-
-//gameLogic
-function kaihangaWheelUpdate() {
-  let segOptions = [];
-  for (let i = 0; i < kaihangaOptions.entries.length; i++) {
-    const col = globalColors.colorOptions[(i + kaihangaOptions.colStart) % globalColors.colorOptions.length];
-    segOptions.push({
-      fillStyle: col,
-      strokeStyle: KadColor.stateAsArray({ colorArray: col, type: "HSL" }),
-      text: kaihangaOptions.entries[i],
-      num: i,
-    });
-  }
-  kaihangaOptions.wheel = new KaihangaWheel({
-    innerR: (kaihangaOptions.canvas.w * 0.1) / 2,
-    outerR: (kaihangaOptions.canvas.w * 0.8) / 2,
-    textFontSize: globalValues.mediaSizes.fontSize,
-    segments: segOptions,
-    lineWeigth: 1,
-    animation: {
-      duration: 2,
-      spins: 4,
-      callbackFinished: kaihangaResult,
-      backgroundColor: globalColors.elements.background,
-    },
-  });
-  kaihangaOptions.wheel.draw();
-}
-
-function kaihangaStart(speed) {
-  if (kaihangaOptions.spinning == true) return;
-  kaihangaOptions.spinning = true;
-  kaihangaCreateRandomSet();
-  kaihangaUpdate();
-  kaihangaOptions.wheel.animation.spins = speed;
-  kaihangaOptions.wheel.animation.duration = 1 / (speed * 10);
-  kaihangaOptions.wheel.startAnimation();
 }
 
 function kaihangaResult(winner) {
@@ -134,6 +73,43 @@ function kaihangaCreateRandomSet() {
 function kaihangaCreateRandomOption() {
   let opt = netsaonaOptions.data.RandomWord.filter((w) => w.length <= kaihangaOptions.maxLength);
   return KadRandom.randomObject(opt);
+}
+
+function kaihangaWheelUpdate() {
+  let segOptions = [];
+  for (let i = 0; i < kaihangaOptions.entries.length; i++) {
+    const col = globalColors.colorOptions[(i + kaihangaOptions.colStart) % globalColors.colorOptions.length];
+    segOptions.push({
+      fillStyle: col,
+      strokeStyle: KadColor.stateAsArray({ colorArray: col, type: "HSL" }),
+      text: kaihangaOptions.entries[i],
+      num: i,
+    });
+  }
+  kaihangaOptions.wheel = new KaihangaWheel({
+    innerR: (kaihangaOptions.canvas.w * 0.1) / 2,
+    outerR: (kaihangaOptions.canvas.w * 0.8) / 2,
+    textFontSize: globalValues.mediaSizes.fontSize,
+    segments: segOptions,
+    lineWeigth: 1,
+    animation: {
+      duration: 2,
+      spins: 4,
+      callbackFinished: kaihangaResult,
+      backgroundColor: globalColors.elements.background,
+    },
+  });
+  kaihangaOptions.wheel.draw();
+}
+
+function kaihangaStart() {
+  if (kaihangaOptions.spinning == true) return;
+  kaihangaOptions.spinning = true;
+  kaihangaCreateRandomSet();
+  kaihangaUpdate();
+  kaihangaOptions.wheel.animation.spins = 5;
+  kaihangaOptions.wheel.animation.duration = 1 / (5 * 10);
+  kaihangaOptions.wheel.startAnimation();
 }
 
 function kaihangaUpdate() {
@@ -194,6 +170,30 @@ function kaihangaUpdate() {
       copy: true,
     });
   }
+}
+
+const caKA = new p5((c) => {
+  c.setup = function () {
+    c.canv = c.createCanvas(kaihangaOptions.canvas.w, kaihangaOptions.canvas.h);
+    c.canv.id("canvasKaihanga");
+    c.canv.parent("#idCanv_kaihanga");
+    c.canv.mousePressed(mousePushedKaihanga);
+    c.colorMode(c.HSL);
+    c.angleMode(c.DEGREES);
+    c.noLoop();
+  };
+
+  c.draw = function () {
+    if (kaihangaOptions.entries.length > 0) {
+      kaihangaOptions.wheel.update();
+      kaihangaOptions.wheel.draw();
+    }
+  };
+}, "#idCanv_kaihanga");
+
+function mousePushedKaihanga() {
+  // let speed = caKA.floor(caKA.map(caKA.mouseX, 0, kaihangaOptions.canvas.w, 1, 18));
+  kaihangaStart();
 }
 
 class KaihangaWheel {
@@ -284,5 +284,76 @@ class KaihangaWheel {
       caKA.pop();
       caKA.pop();
     }
+  }
+}
+
+// cos between 0 an 1 based on t[0,1]
+// f = (1 - Math.cos(t * Math.PI)) / 2;
+function kaihangaUdate() {
+  if (this.pi > Math.PI) {
+    caKA.noLoop();
+    kaihangaResult(this.segments[this.winner]);
+
+    return;
+  }
+  const factor = Math.cos(this.pi + Math.PI) + 1;
+  this.angle = factor * 360 * this.animation.spins + this.winnerAngle;
+  this.pi += 0.01;
+  this.draw();
+}
+
+function kaihangaDraw() {
+  caKA.clear();
+  caKA.push();
+  caKA.translate(kaihangaOptions.canvas.w / 2, kaihangaOptions.canvas.h / 2);
+  caKA.rotate((this.angle + 90) * -1);
+  caKA.noFill();
+  caKA.stroke(globalColors.elements.line);
+  caKA.strokeWeight(this.lineWeight);
+  //Segments
+  let seg = 360 / this.segments.length;
+  for (let i = 0; i < this.segments.length; i++) {
+    let a = seg * i;
+    let b = seg * (i + 1);
+    let xi = caKA.cos(a) * this.innerR;
+    let yi = caKA.sin(a) * this.innerR;
+    let xa = caKA.cos(a) * this.outerR;
+    let ya = caKA.sin(a) * this.outerR;
+    caKA.fill(this.segments[i].fillStyle);
+    caKA.arc(0, 0, this.outerR * 2, this.outerR * 2, a, b);
+    caKA.line(xi, yi, xa, ya);
+    caKA.fill(this.background);
+    caKA.arc(0, 0, this.innerR * 2, this.innerR * 2, a, b);
+    //text
+    caKA.push();
+    caKA.noStroke();
+    caKA.fill(this.segments[i].strokeStyle);
+    caKA.textAlign(caKA.CENTER, caKA.CENTER);
+    caKA.rotate(b - seg / 2);
+    caKA.translate(this.middleR, 0);
+    caKA.text(this.segments[i].text, 0, 0);
+    caKA.pop();
+  }
+  if (kaihangaOptions.entries.length > 0) {
+    caKA.fill(this.background);
+    caKA.circle(0, 0, this.innerR * 2);
+  }
+  caKA.pop();
+  if (kaihangaOptions.entries.length > 0) {
+    caKA.push();
+    let cx = 1.5;
+    let cy = this.innerR * 1.5;
+    caKA.translate(kaihangaOptions.canvas.w * 0.5, kaihangaOptions.canvas.h * 0.5 - this.outerR - this.lineWeight);
+    caKA.noStroke();
+    caKA.fill(globalColors.elements.line);
+    caKA.push();
+    caKA.rotate(135);
+    caKA.rect(-cx, -cx, 2 * cx, cy, cx);
+    caKA.pop();
+    caKA.push();
+    caKA.rotate(225);
+    caKA.rect(-cx, -cx, 2 * cx, cy, cx);
+    caKA.pop();
+    caKA.pop();
   }
 }
